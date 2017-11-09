f5a7a104de4b45aee2efe50b5aa3ade0acc1a401
Raiane Roza <raianeroza@id.uff.br>
2017-11-09 15:51:21 -0200

    Revisado

--- snewton.c
+++ snewton.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 #define N 3
+#define n 4
 
 double f1(double x[N]){
 	return (pow(x[0], 2)) - 81.*(pow(x[1] + 0.1,2)) + sin(x[2]) + 1.06;
@@ -19,24 +20,76 @@ double df(double f(), double x[N], int k){
 	int i, j;
 	
 	for(j=0; j<N; j++)
-	{
 		xb[j]=x[j];
-	}
 	
-	xb[k] = x[k]+h/2.;
+	xb[k] = x[k] + h/2.;			//x modificado
 	
 	M = f(xb);
 	
-	xb[k] = x[k]-h/2.;
+	xb[k] = x[k] - h/2.;			//x modificado
 	
-	M = (M-f(xb))/h;
+	M = (M-f(xb)) / h;				//Calculo da derivada
+	
 	return M;
+}
+double imprimi(double y[N][n])
+{
+
+	int i, j;
+		
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<n; j++)
+			printf("%.2lf\t", y[i][j]);
+		printf("\n");	
+	}
 
 }
 
+double escalonamento(double M[N][N], int v)
+{
+	int i, j;
+	double pivo;
+
+	//Fazendo o escalonamento
+	for(i=v+1; i<N; i++)
+	{
+		pivo = -1 * (M[i][v] / M[v][v]);
+
+		for(j=v; j<N+1; j++)
+			M[i][j] = M[i][j] + (pivo * M[v][j]);
+	}
+
+}
+
+/*
+void Reversa(double **M, int dim)
+{
+	double b, x[dim+1], aux;
+	int i, j;
+
+	b = M[dim-1][dim];
+	x[3] = b / M[dim-1][dim-2];
+
+	for(i=dim-1; i>=0; i--)
+	{
+		b = M[i][dim];
+		for(j=i+1; j<dim; j++)
+			aux += M[i][j]*x[j];
+		x[i] = (b - aux) / M[i][i];
+
+		aux=0;
+	}
+
+	printf("\n--------Solução da Equação--------\n");
+	for(i=0; i<dim; i++)
+		printf("X%d = %lf \t", i+1, x[i]);
+	printf("\n\n");
+}
+*/
 main(int argc, char **argv)
 {
-	double x[N]={0}, x0[N]={0.1,0.1,-0.1}, F[N], J[N][N];
+	double x[N]={0}, x0[N]={0.1,0.1,-0.1}, F[N], J[N][N], y[N][n];
 	double er = 1e-6, tol=0., norm, norma;
 	double (*equacao[N])()={f1,f2,f3};
 	int i, j;
@@ -44,27 +97,31 @@ main(int argc, char **argv)
 	
 	norma=0.;
 	
-	//Calculo do vetor -F(x0)
+	
 	for(i=0; i<N; i++){
 			
-		F[i] = -equacao[i](x0);			
+		F[i] = -equacao[i](x0);			//Calculo do vetor -F(x0)			
 				
-		//printf("\nX[%d]: %lf\t Fx: %lf\n", i, x[i], F[i]);
 		for(j=0; j<N; j++)
-		{
-			J[i][j]=df(equacao[i], x0, j);
-			printf("%lf ", J[i][j]);
-		}
-		printf("\n");
+			J[i][j]=df(equacao[i], x0, j);			//Calculando o jacobiano
 	}
 	
-	//Calculando o jacobiano
+	for(i=0; i<N; i++)
+	{
+		for(j=0; j<N; j++)
+			y[i][j] = J[i][j];
+		y[i][j] = F[i];
+	}
 	
+	imprimi(y);
+
+	for(i=0; i<N; i++)
+	{		
+		if(y[i][i] != 0)
+			escalonamento(y,i);
+	}
+
 	
-		
-	//for(i=0; i<N; i++)
-	//	printf("\nF[%d] = %lf\t", i, F[i]);
-	//printf("\n");
-	//printf("Nº de iterações: %lf\n", tol);
+
 
 } 
